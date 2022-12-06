30145cebad34ed69bcd9be89a680a34939a9cc90
shan <shan.wang@anu.edu.au>
2022-12-06 17:05:08 +1100

    remove unused code

--- pixloc/pixlib/models/two_view_refiner.py
+++ pixloc/pixlib/models/two_view_refiner.py
@@ -99,96 +99,49 @@ class TwoViewRefiner(BaseModel):
         # print('duration:',after_time-start_time)
 
         confidence_count = 2
-        if 'points3D' in data['query'].keys():
-            # need get lidar points
-            p3D_query = data['query']['points3D']
-            if data['query']['points3D_type'] == ['lidar']:
-                confidence_count = 1
         if pred['ref']['confidences'][0].size(1) == 1:
             confidence_count = 1
-
-        if 0: #sat keys
-            # points on satellite image, choose top k confidence on satellite confidence map
-            sat_confidence = merge_confidence_map(pred['ref']['confidences'],confidence_count)  # [B,C,H,W]
-            p2d_sat_key = extract_keypoints(sat_confidence, topk=1024) # [B,N,C]
-
-            if 1:  # debug
-                # show ground confidence
-                sat_confidence = sat_confidence[0].detach().cpu().permute(1, 2, 0)
-                plot_images([sat_confidence], cmaps=mpl.cm.gnuplot2, dpi=50)
-                plt.show()
-                # plot points
-                imq = data['ref']['image'][0].detach().cpu().permute(1, 2, 0)
-                plot_images([imq], dpi=50)  # set to 100-200 for higher res
-                plot_keypoints([p2d_sat_key[0].detach().cpu()], colors='lime')
-                plt.show()
-
-            # sat key from 2d to 3d, assume all points on ground
-            p3d_sat_key = data['ref']['camera'].image2world(p2d_sat_key)  # 2D->3D z unknown
 
         if data['query']['image'].size(-1) > 1224:
             grd_plane_height = 1.65 # kitti
         else:
             grd_plane_height = 1.6 # ford
-        if 'points3D' not in data['query'].keys():
-            # find ground key points from confidence map. top from each grd_img
-            if 'query_3' in data.keys():
-                query_list = ['query', 'query_1', 'query_2', 'query_3']
-            elif 'query_1' in data.keys():
-                query_list = ['query', 'query_1']
+
+        # find ground key points from confidence map. top from each grd_img
+        if 'query_3' in data.keys():
+            query_list = ['query', 'query_1', 'query_2', 'query_3']
+        elif 'query_1' in data.keys():
+            query_list = ['query', 'query_1']
+        else:
+            query_list = ['query']
+        for q in query_list:
+            # find 2d key points from grd confidence map
+            grd_key_confidence = merge_confidence_map(pred[q]['confidences'],confidence_count) #[B,H,W]
+            p2d_grd_key = extract_keypoints(grd_key_confidence)
+
+            # turn grd key points from 2d to 3d, assume points are on ground
+            p3d_grd_key = data[q]['camera'].image2world(p2d_grd_key) # 2D->3D scale unknown
+
+            current_grd_plane_height = (data[q]['T_w2cam'] * torch.zeros(1, 3).to(p3d_grd_key))[
+                                           0, 0, 1] + grd_plane_height
+            if data['query']['image'].size(-1) > 1224:
+                # to pramary camera rotation
+                p3d_grd_cam0 = torch.einsum('...ij,...nj->...ni', data[q]['T_w2cam'].inv().R, p3d_grd_key)
+                # ignore camera rotation
+                cam2plane_R = torch.tensor([[0.9999, -0.0083, -0.0128],[0.0085,0.9999,0.0141],[0.0127,-0.0142,0.9998]]).to(p3d_grd_key)
+                p3d_grd_cam0 = torch.einsum('...ij,...nj->...ni', cam2plane_R, p3d_grd_cam0)
+                depth = current_grd_plane_height / p3d_grd_cam0[:, :, 1]
             else:
-                query_list = ['query']
-            for q in query_list:
-                # find 2d key points from grd confidence map
-                grd_key_confidence = merge_confidence_map(pred[q]['confidences'],confidence_count) #[B,H,W]
-                p2d_grd_key = extract_keypoints(grd_key_confidence)
+                depth = current_grd_plane_height / p3d_grd_key[:, :, 1]
+            p3d_grd_key = depth.unsqueeze(-1) * p3d_grd_key
+            # each camera coordinate to 'query' coordinate
+            p3d_grd_key = data[q]['T_w2cam'].inv()*p3d_grd_key
 
-                if 0: # debug
-                    # show ground confidence
-                    grd_confidence = grd_key_confidence[0].detach().cpu().permute(1, 2, 0)
-                    plot_images([grd_confidence], cmaps=mpl.cm.gnuplot2, dpi=50)
-                    plt.show()
-                    # plot points
-                    imq = data[q]['image'][0].detach().cpu().permute(1, 2, 0)
-                    plot_images([imq], dpi=50)  # set to 100-200 for higher res
-                    plot_keypoints([p2d_grd_key[0].detach().cpu()], colors='lime')
-                    plt.show()
-
-
-
-                # turn grd key points from 2d to 3d, assume points are on ground
-                p3d_grd_key = data[q]['camera'].image2world(p2d_grd_key) # 2D->3D scale unknown
-
-                current_grd_plane_height = (data[q]['T_w2cam'] * torch.zeros(1, 3).to(p3d_grd_key))[
-                                               0, 0, 1] + grd_plane_height
-                if data['query']['image'].size(-1) > 1224:
-                    # to pramary camera rotation
-                    p3d_grd_cam0 = torch.einsum('...ij,...nj->...ni', data[q]['T_w2cam'].inv().R, p3d_grd_key)
-                    # ignore camera rotation
-                    cam2plane_R = torch.tensor([[0.9999, -0.0083, -0.0128],[0.0085,0.9999,0.0141],[0.0127,-0.0142,0.9998]]).to(p3d_grd_key)
-                    p3d_grd_cam0 = torch.einsum('...ij,...nj->...ni', cam2plane_R, p3d_grd_cam0)
-                    depth = current_grd_plane_height / p3d_grd_cam0[:, :, 1]
-                else:
-                    depth = current_grd_plane_height / p3d_grd_key[:, :, 1]
-
-                # # get pitch & roll from init
-                # T_c2r = data['T_q2r_init']@(data[q]['T_w2cam'].inv())
-                # p3d_grd_sat = torch.einsum('...ij,...nj->...ni', T_c2r.R, p3d_grd_key)
-                # depth = current_grd_plane_height/p3d_grd_sat[:, :, 2]
-
-                #depth = current_grd_plane_height / p3d_grd_key[:, :, 1]
-                p3d_grd_key = depth.unsqueeze(-1) * p3d_grd_key
-                # each camera coordinate to 'query' coordinate
-                p3d_grd_key = data[q]['T_w2cam'].inv()*p3d_grd_key
-
-                if 'points3D' not in data['query'].keys():
-                    if q == 'query':
-                        p3D_query = p3d_grd_key
-                    else:
-                        p3D_query = torch.cat([p3D_query, p3d_grd_key], dim=1)
-                else:
-                    p3D_query = torch.cat([p3D_query,p3d_grd_key],dim=1)
-            pred['query']['grd_key_3d'] = p3D_query
+            if q == 'query':
+                p3D_query = p3d_grd_key
+            else:
+                p3D_query = torch.cat([p3D_query, p3d_grd_key], dim=1)
+        pred['query']['grd_key_3d'] = p3D_query
 
 
         T_init = data['T_q2r_init']
@@ -219,45 +172,6 @@ class TwoViewRefiner(BaseModel):
                     F_q_cur = pred[q]['feature_maps'][i]
                     cam_q = pred[q]['camera_pyr'][i]
 
-                    # debug original image
-                    if 0:
-                        fig = plt.figure(figsize=plt.figaspect(0.5))
-                        ax1 = fig.add_subplot(1, 2, 1)
-                        ax2 = fig.add_subplot(1, 2, 2)
-                        color_image1, color_image0 = features_to_RGB(F_ref[0].detach().cpu().numpy(),
-                                                                     F_q_cur[0].detach().cpu().numpy(), skip=1)
-
-                        # grd
-                        p3D_q = data[q]['T_w2cam'] * p3D_query#data['query']['points3D']
-                        p2D, visible = cam_q.world2image(p3D_q)
-                        p2D = p2D.cpu().detach()
-                        p2D = p2D[visible]
-                        for j in range(p2D.shape[0]):
-                            cv2.circle(color_image0, (np.int32(p2D[j][0]), np.int32(p2D[j][1])), 2, (255, 255, 255),
-                                       -1)
-
-                        # sat
-                        p3D_ref_near = data['T_q2r_gt'] * p3D_query#data['query']['points3D']
-                        p3D_ref_near, visible_near = cam_ref.world2image(p3D_ref_near)
-                        p3D_ref_near = p3D_ref_near.cpu().detach()
-                        p3D_ref_near = p3D_ref_near[visible & visible_near]
-
-                        # p3D_ref_far = data['T_q2r_gt'] * (data['query']['points3D']*40)
-                        # p3D_ref_far, visible_far = cam_ref.world2image(p3D_ref_far)
-                        # p3D_ref_far = p3D_ref_far.cpu().detach()
-                        # p3D_ref_far = p3D_ref_far[visible & visible_near]
-
-                        for j in range(p3D_ref_near.shape[0]):
-                            cv2.circle(color_image1, (np.int32(p3D_ref_near[j][0]), np.int32(p3D_ref_near[j][1])), 2,
-                                       (255, 255, 255),
-                                       -1)
-                            # cv2.line(color_image1, (np.int32(p3D_ref_near[j][0]), np.int32(p3D_ref_near[j][1])), \
-                            #          (np.int32(p3D_ref_far[j][0]), np.int32(p3D_ref_far[j][1])), (255, 255, 255), 1)
-
-                        ax1.imshow(color_image0)
-                        ax2.imshow(color_image1)
-                        plt.show()
-
                     p2D_query, visible = cam_q.world2image(data[q]['T_w2cam'] * p3D_query)
                     F_q_cur, mask_cur, _ = opt.interpolator(F_q_cur, p2D_query)
                     mask_cur &= visible
@@ -280,16 +194,9 @@ class TwoViewRefiner(BaseModel):
                     if F_q is None:
                         F_q = F_q_cur * mask_cur[:,:,None]
                         mask = mask_cur
-                        # multi_inputs = mask_cur.type(torch.uint8)
                     else:
                         F_q = F_q_cur * mask_cur[:,:,None] + F_q * mask[:,:,None]
-                        # F_q += F_q_cur * mask_cur[:,:,None]
-                        # multi_inputs = multi_inputs + mask_cur.type(torch.uint8)
                         mask = torch.logical_or(mask, mask_cur)
-
-                # # average for multi inputs
-                # F_q = F_q / (multi_inputs[:, :, None] + 1e-7*~mask[:,:,None])
-                # W_q = W_q / (multi_inputs[:, :, None] + 1e-7*~mask[:,:,None])
 
                 W_ref = pred['ref']['confidences'][i]
                 W_ref_q = (W_ref, W_q, confidence_count)
@@ -302,33 +209,6 @@ class TwoViewRefiner(BaseModel):
                 ## only one query---------------------------------------------------
                 F_q = pred['query']['feature_maps'][i]
                 cam_q = pred['query']['camera_pyr'][i]
-
-                # debug original image
-                if 0:
-                    fig = plt.figure(figsize=plt.figaspect(0.5))
-                    ax1 = fig.add_subplot(1, 2, 1)
-                    ax2 = fig.add_subplot(1, 2, 2)
-                    color_image1, color_image0 = features_to_RGB(F_ref[0].detach().cpu().numpy(), F_q[0].detach().cpu().numpy(), skip=1)
-
-                    # sat
-                    p3D_ref = data['T_q2r_gt'] * p3D_query#data['query']['points3D']
-                    p2D_ref, visible = cam_ref.world2image(p3D_ref)
-                    p2D_ref = p2D_ref.cpu().detach()
-                    for j in range(p2D_ref.shape[1]):
-                        cv2.circle(color_image1, (np.int32(p2D_ref[0][j][0]), np.int32(p2D_ref[0][j][1])), 2, (255, 0, 0),
-                                   -1)
-
-                    p3D_q = data['query']['T_w2cam'] * p3D_query#data['query']['points3D']
-                    p2D, visible = cam_q.world2image(p3D_q)
-                    p2D = p2D.cpu().detach()
-                    # valid = valid & visible
-                    for j in range(p2D.shape[1]):
-                        cv2.circle(color_image0, (np.int32(p2D[0][j][0]), np.int32(p2D[0][j][1])), 2, (255, 0, 0),
-                                   -1)
-
-                    ax1.imshow(color_image0)
-                    ax2.imshow(color_image1)
-                    plt.show()
 
                 p2D_query, visible = cam_q.world2image(data['query']['T_w2cam']*p3D_query)
                 F_q, mask, _ = opt.interpolator(F_q, p2D_query)
@@ -388,29 +268,13 @@ class TwoViewRefiner(BaseModel):
 
     def loss(self, pred, data):
         cam_ref = data['ref']['camera']
-        
-        if 'points3D' not in data['query'].keys():
-            points_3d = pred['query']['grd_key_3d'] 
-        else:
-            points_3d = data['query']['points3D']
-            if 'grd_key_3d' in pred['query'].keys():
-                points_3d = torch.cat([points_3d, pred['query']['grd_key_3d']], dim=1)
-
+        points_3d = pred['query']['grd_key_3d']
         def project(T_q2r):
             return cam_ref.world2image(T_q2r * points_3d)
 
         p2D_r_gt, mask = project(data['T_q2r_gt'])
         p2D_r_i, mask_i = project(data['T_q2r_init'])
         mask = (mask & mask_i).float()
-
-        too_few = torch.sum(mask, -1) < 10
-        if torch.any(too_few):
-            logger.warning('Few points in batch '+str(data['scene']))
-            # logger.warning(
-            #     'Few points in batch '+str([
-            #         (data['scene'][i], data['ref']['index'][i].item(),
-            #          data['query']['index'][i].item())
-            #         for i in torch.where(too_few)[0]]))
 
         def reprojection_error(T_q2r):
             p2D_r, _ = project(T_q2r)
@@ -444,8 +308,6 @@ class TwoViewRefiner(BaseModel):
                 losses['total'] += (poss_loss_weight * pred['pose_loss'][i]/ num_scales).clamp(max=self.conf.clamp_error/num_scales)
 
         losses['reprojection_error'] = err
-        losses['total'] *= (~too_few).float()
-
         losses['reprojection_error/init'] = err_init
 
         return losses
@@ -457,11 +319,6 @@ class TwoViewRefiner(BaseModel):
         def scaled_pose_error(T_q2r):
             err_R, err_t = (T_r2q_gt@T_q2r).magnitude()
             err_lat, err_long = (T_r2q_gt@T_q2r).magnitude_latlong()
-            # if self.conf.normalize_dt:
-            #     err_t /= torch.norm(T_r2q_gt.t, dim=-1)
-            # # change for validate lateral error only, change by shan
-            # # return err_R, err_t
-            #     err_x /= T_r2q_gt.magnitude_lateral()
             return err_R, err_t, err_lat, err_long
 
         metrics = {}
